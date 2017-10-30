d1c69545f0e29669a9481b4907e38869277cd787
ishaankulshrestha <ishaan.kulshrestha@gmail.com>
2017-10-30 14:31:05 +0530

    first commit of project

---
+++ LearningScala/.worksheet/src/com.ishaank.spark.learningscala.tryWorkSheet.scala
@@ -0,0 +1,5 @@
+package com.ishaank.spark.learningscala
+
+object tryWorkSheet {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(106); 
+  println("Welcome to the Scala worksheet")}
+}
